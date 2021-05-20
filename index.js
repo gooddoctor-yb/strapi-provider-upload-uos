6805e7d8a06d55eac5cb223fcb711fdb78bb547a
yb <yb>
2021-05-20 17:34:22 +0800

    update code

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
+const axios = require("axios");
+const qs = require("qs");
+const fs = require("fs");
+const path = require("path");
+const BufferStream = require("./BufferStream");
+
+const url = "http://srv.test.gooddoctor.co.id/uos";
+
 module.exports = {
   init: (providerOptions) => {
-    console.log(
-      "🚀 ~ file: index.js ~ line 3 ~ init ~ providerOptions",
-      providerOptions
-    );
+    console.log("🚀 ~ file: index.js ~ line 3 ~ init ~ providerOptions", providerOptions);
     // init your provider if necessary
 
     return {
       upload: async (file) => {
-        console.log("🚀 ~ file: index.js ~ line 2222123121 ~ upload ~ file", file);
-        console.log(
-          "🚀 ~ file: index.js ~ line 20 ~ upload: ~ providerOptions",
-          providerOptions
-        );
+        console.log("🚀 ~ file: index.js ~ line 14 ~ upload: ~ file", file);
+        const filepath = path.resolve(__dirname + "/1.png");
+        console.log(filepath);
+        fs.readFile(filepath, function (err, data) {
+          if (err) throw err;
 
-        // if (
-        //   config.thumbnails &&
-        //   IMAGE_EXTENSIONS.includes(file.ext.toLowerCase()) &&
-        //   new RegExp(STRAPI_IMAGE_PREFIXES.join("|")).test(file.hash) === false
-        // ) {
-        //   const thumbs = await generateImages(file, config);
+          // Encode to base64
+          // var encodedImage = new Buffer.from(data, "binary").toString("base64");
 
-        //   thumbs.forEach((images) => {
-        //     images.forEach((image) => {
-        //       S3Upload(S3)(
-        //         image,
-        //         `${image.name}_${file.hash}${image.ext}`,
-        //         config
-        //       );
-        //     });
-        //   });
-        // }
+          // Decode from base64
+          // var decodedImage = new Buffer.from(encodedImage, "base64").toString("binary");
 
-        // return S3Upload(S3)(file, `${file.hash}${file.ext}`, config);
+          const queryParams = qs.stringify({
+            p: "cms",
+            s: "0.33737177",
+            h: "b85b3a15ab21807d60aef04cd226cddaa53fbc57",
+            m: "u",
+            q: "12398ada3c16b3c7",
+          });
 
-        // upload the file in the provider
+          // var fileBuffer = new Buffer.from(file.arrayBuffer, "binary");
+          var options = {
+            method: "post",
+            url: url + "?" + queryParams,
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+            body: data,
+          };
+
+          return axios(options)
+            .then(function (response) {
+              console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        });
+
+        // let buffer = Buffer.from(file);
+
+        // var formData = new FormData();
+        // var imagefile = document.querySelector("#file");
+        // formData.append("image", file);
       },
       delete(file) {
         console.log("🚀 ~ file: index.js ~ line 15 ~ delete ~ file", file);
